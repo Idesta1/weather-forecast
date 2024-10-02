5626301550b44c276aaf48af2aa1619c4765f38c
Idesta1 <116448334+Idesta1@users.noreply.github.com>
2024-10-02 21:49:56 +0200

    removed html div

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let temperatureIconElement= document.querySelector("#icon");
-
+let date = new Date();
 
 temperatureElement.innerHTML=Math.round(temperature);
 cityElement.innerHTML=response.data.city;
@@ -14,34 +14,12 @@ descriptionElement.innerHTML=response.data.condition.description;
 humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
 temperatureIconElement.innerHTML=`<img src="${response.data.condition.icon_url}" class="temperature-icon"/>`;
-}
-
-function searchCity(city){
-let apiKey="8e95b71ct703a6fac0458b466ebo4b0b";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(changeWeather);
-}
-function showSubmit(event){
-    event.preventDefault();
-  let searchInput=document.querySelector("#search-input-app");
-
-   
-   searchCity(searchInput.value);
-   
-  
-
-}
-
-let searchFormElement=document.querySelector("#search-form-app");
-searchFormElement.addEventListener ("submit", showSubmit);
-
-searchCity("city");
-
-let date = new Date();
-weatherElement=document.querySelector("#current-time");
 weatherElement.innerHTML= formatDate(date);
+}
+weatherElement=document.querySelector("#current-time");
 
 function formatDate(date){
+
 let days = [
     "Sunday",
     "Monday",
@@ -62,6 +40,56 @@ let days = [
 
   return  `${day} ${hours}:${minutes}`;
 }
+
+
+function searchCity(city){
+let apiKey="8e95b71ct703a6fac0458b466ebo4b0b";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(changeWeather);
+}
+function showSubmit(event){
+    event.preventDefault();
+  let searchInput=document.querySelector("#search-input-app");
+
+   
+   searchCity(searchInput.value);
+   
+}
+
+function displayForecast() {
+
+let days = ["Tue","Wed","Thu","Fri","Sat"];
+let forecastHtml = "";
+
+days.forEach(function (day) {
+  forecastHtml =
+  forecastHtml +
+ ` 
+ <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-temperature">
+          <div class="weather-forecast-temp"><strong>21°</strong></div>
+            <div class="weather-forecast-temp">16°</div>
+      </div>
+    </div>
+    `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+
+let searchFormElement=document.querySelector("#search-form-app");
+searchFormElement.addEventListener ("submit", showSubmit);
+
+searchCity("city");
+displayForecast();
+
+
+
+
 
 
  
